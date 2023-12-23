9cd17715577e5c03fe18e352d90c36c867d60702
abuhasanmahmud <mahmud.me420@gmail.com>
2023-12-23 23:33:52 +0600

    s

--- app/page.js
+++ app/page.js
@@ -1,113 +1,249 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  //add two number
+  const addNumbers = () => {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+      const sum = num1 + num2;
+      setResult(`Result: ${sum}`);
+    } else {
+      setResult("Please enter valid numbers");
+    }
+  };
+
+  //subtrac number
+  const [subnumber1, setsubNumber1] = useState("");
+  const [subnumber2, setsubNumber2] = useState("");
+  const [subresult, setsubResult] = useState("");
+
+  const subtractNumbers = () => {
+    const num1 = parseFloat(subnumber1);
+    const num2 = parseFloat(subnumber2);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+      const difference = num1 - num2;
+      setsubResult(`Result: ${difference}`);
+    } else {
+      setsubResult("Please enter valid numbers");
+    }
+  };
+
+  //multiply two number
+  const [mulNumber1, setmulNumber1] = useState("");
+  const [mulNumber2, setmulNumber2] = useState("");
+  const [mulResult, setmulResult] = useState("");
+
+  const multiplyNumbers = () => {
+    const num1 = parseFloat(mulNumber1);
+    const num2 = parseFloat(mulNumber2);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+      const product = num1 * num2;
+      setmulResult(`Result: ${product}`);
+    } else {
+      setmulResult("Please enter valid numbers");
+    }
+  };
+
+  //modulas two number
+  const [monumber1, setmoNumber1] = useState("");
+  const [monumber2, setmoNumber2] = useState("");
+  const [moresult, setmoResult] = useState("");
+
+  const calculateModulus = () => {
+    const num1 = parseFloat(monumber1);
+    const num2 = parseFloat(monumber2);
+
+    if (!isNaN(num1) && !isNaN(num2) && num2 !== 0) {
+      const modulus = num1 % num2;
+      setmoResult(`Result: ${modulus}`);
+    } else if (num2 === 0) {
+      setmoResult("Cannot divide by zero");
+    } else {
+      setmoResult("Please enter valid numbers");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="divide-y divide-blue-200">
+        <div className="bg-gray-200 py-4 ">
+          <h1 className="flex justify-center text-3xl text-blue-700 ">শিখন অভিজ্ঞতা- 4</h1>
+          <p className="flex justify-center text-2xl my-1">সমস্যার সমাধান চাই, প্রোগ্রামিং জুড়ি নাই</p>
+          <p className="flex justify-center text-2xl">
+            সেশন -7:দুটি সংখ্যার ইনপুট নিয়ে করি যোগ, বিয়োগ গুন ভাগ এবং মডুলাস
+          </p>
+        </div>
+
+        <div className="px-10 bg-gray-200 divide-y divide-blue-200">
+          {/* add two number */}
+          <div className="bg-gray-200 px-6 py-5 shadow-lg hover:shadow-sm">
+            <h2 className="my-2 text-2xl font-bold">Add Two Numbers</h2>
+            <label htmlFor="number1" className="mr-2 text-xl font-bold">
+              Number 1:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="number1"
+              value={number1}
+              onChange={(e) => setNumber1(e.target.value)}
+              required
             />
-          </a>
+
+            <label htmlFor="number1" className="mx-4 text-xl font-bold">
+              Number 2:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="number2"
+              value={number2}
+              onChange={(e) => setNumber2(e.target.value)}
+              required
+            />
+
+            <button
+              className="ml-4 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={addNumbers}
+            >
+              Add Number
+            </button>
+
+            <p className="text-3xl font-bold py-3 text-red-600" id="result">
+              {result}
+            </p>
+          </div>
+
+          {/* Subtract  two number */}
+          <div className="bg-gray-200 px-6 py-5 shadow-lg hover:shadow-sm">
+            <h2 className="my-2 text-2xl font-bold">Subtract Two Numbers</h2>
+            <label htmlFor="number1" className="mr-2 text-xl font-bold">
+              Number 1:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="subnumber1"
+              value={subnumber1}
+              onChange={(e) => setsubNumber1(e.target.value)}
+              required
+            />
+
+            <label htmlFor="number1" className="mx-4 text-xl font-bold">
+              Number 2:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="subnumber2"
+              value={subnumber2}
+              onChange={(e) => setsubNumber2(e.target.value)}
+              required
+            />
+
+            <button
+              className="ml-4 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={subtractNumbers}
+            >
+              Subtract Number
+            </button>
+
+            <p className="text-3xl font-bold py-3 text-red-600" id="result">
+              {subresult}
+            </p>
+          </div>
+
+          {/*  tow number multiply */}
+          <div className="bg-gray-200 px-6 py-5 shadow-lg hover:shadow-sm">
+            <h2 className="my-2 text-2xl font-bold">Multiply Two Numbers</h2>
+            <label htmlFor="number1" className="mr-2 text-xl font-bold">
+              Number 1:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="mulNumber1"
+              value={mulNumber1}
+              onChange={(e) => setmulNumber1(e.target.value)}
+              required
+            />
+
+            <label htmlFor="number1" className="mx-4 text-xl font-bold">
+              Number 2:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="mulNumber2"
+              value={mulNumber2}
+              onChange={(e) => setmulNumber2(e.target.value)}
+              required
+            />
+
+            <button
+              className="ml-4 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={multiplyNumbers}
+            >
+              Multiply Number
+            </button>
+
+            <p className="text-3xl font-bold py-3 text-red-600" id="result">
+              {mulResult}
+            </p>
+          </div>
+
+          {/* modulas tow number  */}
+          <div className="bg-gray-200 px-6 py-5">
+            <h2 className="my-2 text-2xl font-bold">Modulas Two Numbers</h2>
+            <label htmlFor="number1" className="mr-2 text-xl font-bold">
+              Number 1:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="monumber1"
+              value={monumber1}
+              onChange={(e) => setmoNumber1(e.target.value)}
+              required
+            />
+
+            <label htmlFor="number1" className="mx-4 text-xl font-bold">
+              Number 2:
+            </label>
+            <input
+              className="p-1 rounded-lg"
+              type="number"
+              id="monumber2"
+              value={monumber2}
+              onChange={(e) => setmoNumber2(e.target.value)}
+              required
+            />
+
+            <button
+              className="ml-4 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={calculateModulus}
+            >
+              Modulus Number
+            </button>
+
+            <p className="text-3xl font-bold py-3 text-red-600" id="result">
+              {moresult}
+            </p>
+          </div>
+
+          {/*  */}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
